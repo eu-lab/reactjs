b6bccf8efcde008682defe84cebd22d7ea27c918
pixies <pixies@pelivre.org>
2016-11-18 12:56:52 -0300

    modificacoes - index.js

--- exemplo1/src/index.js
+++ exemplo1/src/index.js
@@ -1,9 +1,48 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+
+function Menu(){
+	return (
+		<div>
+		<li><a href="{home}">Início</a></li>
+	    <li><a href="{sobre}">Sobre</a></li>
+	    <li><a href="{reactjs}">ReactJS</a></li>
+	    </div>
+	    )
+}
+
+
+function NavBar() {
+	return (
+		
+	  <nav>
+	    <div className="nav-wrapper">
+	      <a href="#" className="brand-logo center">Logo</a>
+	      <ul id="nav-mobile" className="left hide-on-med-and-down">
+	        <Menu />
+	      </ul>
+	    </div>
+	  </nav>
+
+
+	);
+}
+
+function Content() {
+	return (
+		<div className="container">
+			<p>Ola</p>
+		</div>
+	);
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+		(<NavBar />),
+		document.getElementById('nav')
+	);
+
+CVBO
+
+
